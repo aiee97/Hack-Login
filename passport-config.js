6563c51226b1520ab10c89e9f180671da8a91ede
junliang1127 <75826478+junliang1127@users.noreply.github.com>
2021-09-30 14:08:34 +0800

    User Check

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,29 @@
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
 function initialise(passport) {
-    passport.use(new LocalStrategy)
+    const authenticateUser = (username, password, done) => {
+        const user = getUserByUsername(username)
+        if (user == null){
+            return done(null, false, {message: 'Username not found'})
+        }
+
+        try{
+            if (await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            }else{
+                return done(null, false, {message: 'Password incorrect'})
+            }
+        } catch(e){
+            return done(e)
+        }
+
+    }
+    passport.use(new LocalStrategy({usernameField:'username'}), 
+    authenticateUser)
+
+    passport.serializeUser((user, done)=>{  })
+    passport.deserializeUser((id, done)=>{  })
 }
+
+module.exports = initialize
